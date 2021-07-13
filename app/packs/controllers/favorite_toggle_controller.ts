5729e39f3d95ce2bb32ba053fd28bb1ceba94d7f
Jason Force <jason.r.force@gmail.com>
2021-07-13 11:21:19 -0500

    Finish CH-3 Stimulus

---
+++ app/packs/controllers/favorite_toggle_controller.ts
@@ -0,0 +1,43 @@
+import { Controller } from 'stimulus'
+
+export default class FavoriteToggleController extends Controller {
+  static classes = ['hidden']
+  hiddenClass: string
+
+  static targets = ['elementToHide', 'elementWithText']
+  elementToHideTarget: HTMLElement
+  elementWithTextTarget: HTMLElement
+
+  static values = { visible: Boolean }
+  visibleValue: boolean
+
+  connect(): void {
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  toggle(): void {
+    this.flipState()
+  }
+
+  flipState(): void {
+    this.visibleValue = !this.visibleValue
+  }
+
+  visibleValueChanged(): void {
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  updateHiddenClass(): void {
+    this.elementToHideTarget.classList.toggle(this.hiddenClass, !this.visibleValue)
+  }
+
+  newText(): string {
+    return this.visibleValue ? 'Hide' : 'Show'
+  }
+
+  updateText(): void {
+    this.elementWithTextTarget.innerText = this.newText()
+  }
+}
